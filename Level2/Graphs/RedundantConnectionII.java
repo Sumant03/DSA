62f7848f82231ce05176629fa1a3679cdd9a2477
Sumant <sumant.sharma.nonu@gmail.com>
2021-11-23 23:57:30 +0530

    up

--- Level2/Graphs/RedundantConnectionII.java
+++ Level2/Graphs/RedundantConnectionII.java
@@ -1,20 +1,26 @@
-//almost done , only the union vala code is left ;
+// import java.util.*;
+// import java.io.*;
 
+// public class Main {
+//   public static void main(String[] args) throws NumberFormatException, IOException {
+//     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//     String[] st = br.readLine().split(" ");
+//     int n = Integer.parseInt(st[0]);
 
+//     int[][] pos = new int[n][2];
+//     for (int i = 0; i < n; i++) {
+//       st = br.readLine().split(" ");
+//       pos[i][0] = Integer.parseInt(st[0]);
+//       pos[i][1] = Integer.parseInt(st[1]);
+//     }
 
-
-// package Level2.Graphs;
-
-// public class RedundantConnectionII {
+//     int[] ans = findRedundantDirectedConnection(pos);
+//     System.out.println(ans[0] + " " + ans[1]);
+//   }
+//   static  int[] par;
+//   static  int[] rank;
     
-// }
-
-
-// class Solution {
-//     int[] par;
-//     int[] rank;
-    
-//     public int[] findRedundantDirectedConnection(int[][] edges) {
+//     public static  int[] findRedundantDirectedConnection(int[][] edges) {
 //         int n=edges.length;
         
     
@@ -22,12 +28,12 @@
 //         int[] in=new int[n];
 //         Arrays.fill(in,-1);
         
-//         int[] b1=-1;
-//         int[] b2=-1;
+//         int b1=-1;
+//         int b2=-1;
         
 //         for(int i=0;i<edges.length;i++){
-//             int u=edges[i][0];
-//             int v=edges[i][1];
+//             int u=edges[i][0]; u--;
+//             int v=edges[i][1]; v--;
             
 //             if(in[v]==-1){
 //                 in[v]=i;
@@ -41,12 +47,12 @@
 //         }
 //         if(b1==-1){
 //             //case 1
-//            int ei= dsu();
+//             int ei= dsu(-1,edges);
 //             return edges[ei];
 //         }else{
 //             // case 2, case 3
             
-//           int ei=  dsu(b2);
+//           int ei=  dsu(b2,edges);
             
 //             if(ei==-1){
 //                 return edges[b2];
@@ -56,17 +62,17 @@
 //         }
 //     }
     
-//     public int find(int x){
+//     public static int find(int x){
 //         if(par[x]==x){
 //             return x;
 //         }else{
 //             int ans=find(par[x]);
 //             par[x]=ans;
 //             return ans;
-//        }
+//         }
 //     }
     
-//     public boolean union(int u,int v){
+//     public static  boolean union(int u,int v){
 //         int lu=find(u);
 //         int lv=find(v);
         
@@ -79,13 +85,14 @@
 //             par[lu]=lv;
 //             rank[lv]++;
 //         }
+// return true;
 //         }else{
 //             return false;
 //         }
         
 //     }
 //     //black listed index 
-//     public static int dsu(int bli,int[][],edges){
+//     public static int dsu(int bli,int[][] edges){
 //         int n=edges.length;
 //         par=new int[n];
 //         rank=new int[n];
@@ -115,4 +122,6 @@
 //         return -1;
         
 //     }
+
+
 // }
