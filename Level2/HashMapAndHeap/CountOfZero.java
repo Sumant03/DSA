7d4aac8dd5398fbea6f9960df305861e1a6b761e
Sumant <sumant.sharma.nonu@gmail.com>
2021-11-30 17:49:39 +0530

    up

---
+++ Level2/HashMapAndHeap/CountOfZero.java
@@ -0,0 +1,48 @@
+// public import java.util.*;
+
+// public class Main {
+
+// 	public static int solution(int[] arr) {
+// 		// write your code here
+//     HashMap<Integer,Integer> map=new HashMap<>();
+// 	int ps=0;
+//         int count=0;
+        
+// 		map.put(0,1);
+		
+// 		for(int i=0;i<arr.length;i++){
+// 		    ps+=arr[i];
+		    
+// 		    if(map.containsKey(ps)==false){
+// 		        map.put(ps,1);
+// 		    }
+// 		    else{
+// 		        int val=map.get(ps);
+// 		       map.put(ps,val+1);
+// 		       count+=val;
+// 		    }
+		    
+		        
+		       
+// 		    }
+		
+		
+        
+// 		return count;
+// 	}
+	
+// 	public static void main(String[] args) {
+// 		Scanner scn = new Scanner(System.in);
+// 		int n = scn.nextInt();
+// 		int[] arr = new int[n];
+// 		for (int i = 0; i < n; i++) {
+// 			arr[i] = scn.nextInt();
+// 		}
+// 		System.out.println(solution(arr));
+
+// 	}
+
+// }
+// class CountOfZero {
+    
+// }
