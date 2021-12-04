9ca33a242023c0c4cb9a0b564ebd7aa33d8efe22
Sumant <sumant.sharma.nonu@gmail.com>
2021-12-04 18:12:05 +0530

    up

--- Level2/HashMapAndHeap/largestEqualZeroOnetwo.java
+++ Level2/HashMapAndHeap/largestEqualZeroOnetwo.java
@@ -1,3 +1,51 @@
-public class largestEqualZeroOnetwo {
+
+import java.util.*;
+
+public class largestEqualZeroOnetwo{
+
+    public static int solution(int[] arr) {
+        // write your code here
+    HashMap<String,Integer> map=new HashMap<>();//(c1-c0)-(c2-c1) vs first occ;
     
+    map.put("0 0",-1);
+    int ln=0;
+    int c0=0;
+    int c1=0;
+    int c2=0;
+    
+    for(int i=0;i<arr.length;i++){
+      
+      if(arr[i]==0){
+          c0++;
+      }else if(arr[i]==1){
+          c1++;
+      }else{
+          c2++;
+      }
+      String key=(c1-c0)+" "+(c2-c1);
+      
+       
+     if(map.containsKey(key)==false){
+         map.put(key,i);
+     }else{
+         int len=i-map.get(key);
+         if(ln<len){
+             ln=len;
+         }
+     }
+    }
+    
+    return ln;
+    }
+    
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scn.nextInt();
+        }
+        System.out.println(solution(arr));
+    }
+
 }
