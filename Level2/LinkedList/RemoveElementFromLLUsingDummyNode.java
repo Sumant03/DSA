6bb7ab0f9a15c3d022e146cfea6191643fca5d3d
Sumant <sumant.sharma.nonu@gmail.com>
2022-01-02 00:32:09 +0530

    Create RemoveElementFromLLUsingDummyNode.java

---
+++ Level2/LinkedList/RemoveElementFromLLUsingDummyNode.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy=new ListNode(-1);
+        dummy.next=head;
+        ListNode temp=dummy;
+        
+        if(head==null||(head.next==null&&head.val==val)){
+            return null;
+        }
+        
+        while(temp!=null&&temp.next!=null){
+            
+            if(temp.next.val==val){
+                temp.next=temp.next.next;
+            }else{
+                temp=temp.next;
+            }
+     
+                 
+    }
+     return dummy.next;
+}
+}
