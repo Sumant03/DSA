be9f99c5d374862670754aff203ae468a7d94a39
Sumant <sumant.sharma.nonu@gmail.com>
2022-01-04 13:32:40 +0530

    Time: 20 ms (11.80%), Space: 69.3 MB (23.53%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.java
+++ 234-palindrome-linked-list/234-palindrome-linked-list.java
@@ -10,69 +10,43 @@
  */
 class Solution {
     
-         public ListNode reverseList(ListNode head) {
+    static int size;
+    
+    static ListNode leftNode;
+          
+
+    
+    public boolean IsPalindromeHelper(ListNode node,int idx){
         
-        ListNode curr=head;
-        ListNode prev=null;
-        
-        while(curr!=null){
-            //backup
-            ListNode n=curr.next;
-            
-            //connection
-           
-            curr.next=prev;
-                
-                //move;
-                 prev=curr;
-                curr=n;
-                
-            
+        if(node==null){
+            return true;
         }
-        return prev;
         
-    }
-    
-    
-         public ListNode middleNode(ListNode head) {
-         if(head==null){
-             return head;
-         }
-           
-           
-            ListNode fast=head;
-            ListNode slow=head;
+       boolean ans= IsPalindromeHelper(node.next,idx+1);
+        if(idx>=size/2){
+            ListNode right=node;
             
-            while(fast.next!=null&&fast.next.next!=null){
-                
-                fast=fast.next.next;
-                slow=slow.next;
-                
-            }
-            return slow;
-        
-    }
-    
-    
-         public boolean isPalindrome(ListNode head) {
-        
-        ListNode mid=middleNode(head);
-        ListNode nh=mid.next;
-        
-        
-        mid.next=null;
-        
-        ListNode rh=reverseList(nh);
-        ListNode lh=head;
-        
-        while(rh!=null){
-            
-            if(lh.val!=rh.val){
+            if(right.val!=leftNode.val){
                 return false;
             }
-            lh=lh.next;
-            rh=rh.next;
+            leftNode=leftNode.next;
+            
         }
-        return true;
+        
+        return ans;
+    }
+         
+    
+    
+    public boolean isPalindrome(ListNode head) {
+             size=0;
+             ListNode temp=head;
+             while(temp!=null){
+                 temp=temp.next;
+                 size++;
+             }
+             
+             leftNode =head;
+             return IsPalindromeHelper(head,1);
     }
 }
