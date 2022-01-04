1961c057d3b74c2d11dbaf6bc450524842127567
Sumant <sumant.sharma.nonu@gmail.com>
2022-01-04 13:35:11 +0530

    Time: 8 ms (38.77%), Space: 115.9 MB (12.63%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.java
+++ 234-palindrome-linked-list/234-palindrome-linked-list.java
@@ -10,43 +10,39 @@
  */
 class Solution {
     
-    static int size;
-    
-    static ListNode leftNode;
-          
+     public boolean isPalindrome(ListNode head) {
+    if (head == null || head.next == null) {
+        return true;
+    }
 
-    
-    public boolean IsPalindromeHelper(ListNode node,int idx){
-        
-        if(node==null){
-            return true;
-        }
-        
-       boolean ans= IsPalindromeHelper(node.next,idx+1);
-        if(idx>=size/2){
-            ListNode right=node;
-            
-            if(right.val!=leftNode.val){
-                return false;
-            }
-            leftNode=leftNode.next;
-            
-        }
-        
-        return ans;
+    ListNode slow = head;
+    ListNode fast = head;
+    // fast.next ---> for even list
+    // fast.next.next ---> for odd list
+    while (fast.next != null && fast.next.next != null) {
+        slow = slow.next;
+        fast = fast.next.next;
     }
-         
-    
-    
-    public boolean isPalindrome(ListNode head) {
-             size=0;
-             ListNode temp=head;
-             while(temp!=null){
-                 temp=temp.next;
-                 size++;
-             }
-             
-             leftNode =head;
-             return IsPalindromeHelper(head,1);
+    slow.next = reverse(slow.next); // here we get the mid+1 of the LL  for reverse
+    slow = slow.next; // this slow points to next of the mid of the list
+    while (slow != null) {
+        if (head.val != slow.val) {
+            return false;
+        }
+        head = head.next;
+        slow = slow.next;
     }
+    return true;
+}
+
+     public ListNode reverse(ListNode head) {
+    ListNode prev = null, nxt = null;
+    while (head != null) {
+        nxt = head.next;
+        head.next = prev;
+        prev = head;
+        head = nxt;
+    }
+    return prev;
+}
 }
