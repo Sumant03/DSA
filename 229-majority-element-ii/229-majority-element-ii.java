4a08b8fa4b28e8c906587ad05dab3dcf74b6d5d5
Sumant <sumant.sharma.nonu@gmail.com>
2022-01-08 14:32:19 +0530

    Time: 2 ms (73.80%), Space: 46.4 MB (22.34%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.java
+++ 229-majority-element-ii/229-majority-element-ii.java
@@ -1,23 +1,55 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        List<Integer> list=new ArrayList<>();
+        List<Integer> ans=new ArrayList<>();
+        int val1=nums[0];
+        int c1=1;
         
-        for(int i=0;i<nums.length;i++){
+        int val2=0;
+        int c2=0;
+        
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]==val1){
+                c1++;
+            }else if(nums[i]==val2){
+                c2++;
+            }else{
+               if(c1==0){
+                   val1=nums[i];
+                   c1=1;
+               }else if(c2==0){
+                   val2=nums[i];
+                   c2=1;
+               }else{
+                c1--;
+                c2--;
+               }
+            }
             
-            int val=nums[i];
             
-            int freq=map.getOrDefault(val,0)+1;
-            map.put(val,freq);
+            
         }
         
-        for(int val:map.keySet()){
-            
-            if(map.get(val)>nums.length/3){
-                list.add(val);
+        int f1=0;
+        int f2=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==val1){
+                f1++;
+            }
+             else if(nums[i]==val2){
+                f2++;
             }
         }
         
-        return list;
+        
+        if(f1>nums.length/3){
+            ans.add(val1);
+        }
+        if(f2>nums.length/3){
+            ans.add(val2);
+        }
+        
+        
+        return ans;
+  
     }
 }
