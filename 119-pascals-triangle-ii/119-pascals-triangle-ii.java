ce3c98030924d9bc36abbb61530834b5406be277
Sumant <sumant.sharma.nonu@gmail.com>
2022-01-18 01:02:54 +0530

    Time: 1 ms (81.46%), Space: 38.2 MB (39.61%) - LeetHub

---
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<Integer> getRow(int n) {
+       
+        
+        List<Integer> ans=new ArrayList<>();
+        long val=1;
+        
+        ans.add(1);
+        for(int r=0;r<n;r++){
+            val=(val*(n-r))/(r+1);
+            
+            ans.add((int)val);
+        }
+        return ans;
+    }
+}
