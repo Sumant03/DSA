52c001d77b43d1a60ef89e84e8efabcd9a3c9db6
Sumant <sumant.sharma.nonu@gmail.com>
2022-01-25 22:36:25 +0530

    Time: 3 ms (27.48%), Space: 51.3 MB (39.75%) - LeetHub

--- 941-valid-mountain-array/941-valid-mountain-array.java
+++ 941-valid-mountain-array/941-valid-mountain-array.java
@@ -1,30 +1,23 @@
 class Solution {
     public boolean validMountainArray(int[] arr) {
-        
-        int idx1=0;
-        int idx2=0;
-        int i=1;
-    while(i<arr.length&&i+1<arr.length&&arr[i]>arr[i-1]){
-             idx1++;
+
+        int i=0;
+        while(i<arr.length&&i+1<arr.length&&arr[i]<arr[i+1]){
              i++;
-       }
-        System.out.println(arr[idx1]);
-        
-        
-        int j=arr.length-2;
-        while(j>=0&&arr[j]>arr[j+1]){
-            idx2++;
-            j--;
         }
-        System.out.println(arr[idx2]);
-        
-        if(idx1>0&&idx2>0&&(idx1+idx2+1)==arr.length){
-            return true;
-        }
-        else{
-            System.out.println("3");
+
+        if(i==0||i+1>=arr.length){
             return false;
         }
+        
+        while(i<arr.length&&i+1<arr.length){
+            if(arr[i]<=arr[i+1]){
+                return false;
+            }
+            i++;
+        }
+
+        return true;
         
     }
 }
