9f19482cfb138c1dc24d181d43f9d49a8a423923
Sumant <sumant.sharma.nonu@gmail.com>
2022-01-31 18:34:57 +0530

    Time: 38 ms (39.53%), Space: 43 MB (71.47%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.java
+++ 875-koko-eating-bananas/875-koko-eating-bananas.java
@@ -1,33 +1,44 @@
 class Solution {
-    public int minEatingSpeed(int[] piles, int H) {
-        int low = 1, k = 0;
-        int high=getMax(piles);
-        while (low <= high) {
-            k = (low + high) / 2;
-            int h = 0;
-            for (int i = 0; i < piles.length; i ++) 
-                h += Math.ceil(1.0 * piles[i] / k);
-            if (h > H)
-                low = k + 1;
-            else
-                high = k - 1;
+    public int minEatingSpeed(int[] piles, int h) {
+        
+        int max=piles[0];
+        
+        for(int i=1;i<piles.length;i++){
+            max=Math.max(max,piles[i]);
         }
-        return low;
-
-
-    }
-    
-    public static int getMax(int[] arr){
         
-        int max=Integer.MIN_VALUE;
+        if(h==piles.length){
+            return max;
+        }
         
-        for(int i=0;i<arr.length;i++){
+        int lo=0,hi=max;
+        int ans=0;
+        
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
             
-            if(arr[i]>max){
-                max=arr[i];
+            if(isPossible(piles,mid,h)==true){
+                //mid jitni speed rkhte huye kya me 
+                //piles ke saare banana ko h hours mein khaaa skte hu 
+                ans=mid;
+                hi=mid-1;
+            }else{
+                lo=mid+1;
             }
+        
         }
-        return max;
+    return ans;
     }
     
+    public static boolean isPossible(int[] piles,int mid,int h){
+        int t=0;
+        
+        for(int val:piles){
+            
+            t+=Math.ceil(1.0*val/mid);
+            
+            
+        }
+    return t<=h;
+    }
 }
