1f1bb99a1a3a082716a5f1b15719176bc85fc4d1
Sumant <sumant.sharma.nonu@gmail.com>
2022-02-04 01:07:35 +0530

    Time: 194 ms (42.38%), Space: 56.8 MB (28.00%) - LeetHub

--- 454-4sum-ii/454-4sum-ii.java
+++ 454-4sum-ii/454-4sum-ii.java
@@ -1,24 +1,27 @@
 class Solution {
     public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
-        HashMap<Integer,Integer> map = new HashMap<>();
+       HashMap<Integer,Integer> map=new HashMap<>();
         
-        //find the frequency of remaining sum
         for(int i=0;i<nums1.length;i++){
-            for(int j=0;j<nums2.length;j++){
-                int sum = nums1[i] + nums2[j];
-                map.put(-sum , map.getOrDefault(-sum , 0) + 1);
+            for( int j=0;j<nums2.length;j++){
+                int sum=nums1[i]+nums2[j];
+                
+                int val=map.getOrDefault(sum,0)+1;
+                map.put(sum,val);
             }
         }
         
-        int count = 0;
-        //if we already have the remaining sum, increase the count
+        int count=0;
+        
         for(int i=0;i<nums3.length;i++){
             for(int j=0;j<nums4.length;j++){
-                int sum = nums3[i] + nums4[j];
-                count += map.getOrDefault(sum , 0);
+                int sum=nums3[i]+nums4[j];
+                
+        
+                    count+=map.getOrDefault(0-sum,0);
+                
             }
         }
-        
         return count;
     }
 }
