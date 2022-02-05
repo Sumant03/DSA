3b35476460cb5d63609cfb55ed7090ac6ec5d3f9
Sumant <sumant.sharma.nonu@gmail.com>
2022-02-05 19:15:09 +0530

    Time: 4 ms (82.77%), Space: 43.8 MB (58.47%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
@@ -5,62 +5,53 @@
  *     ListNode next;
  *     ListNode() {}
  *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }  
  * }
  */
 class Solution {
-    public static class Pair implements Comparable<Pair> {
+    public class Pair implements Comparable<Pair>{
         ListNode node;
         
-          Pair() {
-
+        Pair(){
+            
         }
         
         Pair(ListNode node){
             this.node=node;
         }
+        
         public int compareTo(Pair o){
             return this.node.val-o.node.val;
         }
-        
     }
+    
     public ListNode mergeKLists(ListNode[] lists) {
-        
+        if(lists==null||lists.length==0){
+            return null;
+        }      
         ListNode dh=new ListNode(-1);
         ListNode dt=dh;
-        
         PriorityQueue<Pair> pq=new PriorityQueue<>();
         
         for(ListNode node:lists){
-            
-            if(node==null){
-                continue;
+            if(node!=null){
+            pq.add(new Pair(node));
             }
-            
-            Pair p=new Pair(node);
-                pq.add(p);
         }
         
         while(pq.size()>0){
-            
-            Pair pr=pq.remove();
-            ListNode node=pr.node;
-            
-            dt.next=node;
+            Pair p=pq.remove();
+            ListNode cn=p.node;
+            dt.next=cn;
             dt=dt.next;
             
-            
-            
-            if(node.next!=null){
-                Pair p=new Pair(node.next);
-                pq.add(p);
-            }
-            
+               if(cn!=null&&cn.next!=null){
+                   pq.add(new Pair(cn.next));
+               }
             
         }
         
         return dh.next;
-        
         
         
     }
