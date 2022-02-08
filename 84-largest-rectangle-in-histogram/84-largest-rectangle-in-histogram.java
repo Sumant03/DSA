42182d7bc97580aee2c9126af809327151ded26e
Sumant <sumant.sharma.nonu@gmail.com>
2022-02-08 19:46:49 +0530

    Time: 331 ms (5.03%), Space: 95.2 MB (10.12%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
@@ -1,24 +1,61 @@
 class Solution {
-    public int largestRectangleArea(int[] heights) {
-     if (heights == null || heights.length == 0) {
+    public int largestRectangleArea(int[] arr) {
+     if (arr == null ||arr.length == 0) {
             return 0;
         }
-        Stack<Integer> stack = new Stack<>();
-        int ans = 0;
-        int n = heights.length;
-        for (int i = 0; i <= n; i++) {
-            int height = i == n ? -1 : heights[i];
-            while (!stack.isEmpty() && heights[stack.peek()] > height) {
-                int leftBound = stack.pop();
-                if (stack.isEmpty()) {
-                    ans = Math.max(ans, i * heights[leftBound]);
-                } else {
-                    ans = Math.max(ans, (i - stack.peek() - 1) * heights[leftBound]);
-                }
-            }
-            stack.push(i);
-        }
-        return ans;
         
+    int n=arr.length;    
+    int[] rb=new int[n];
+    Stack<Integer> st=new Stack<>();
+    st.push(n-1);
+    rb[n-1]=n;
+    
+    for(int i=n-2;i>=0;i--){
+        
+        while(st.size()>0&&arr[i]<=arr[st.peek()]){
+            st.pop();
+        }
+        
+        
+        if(st.size()==0){
+            rb[i]=n;
+        }else{
+            rb[i]=st.peek();
+        }
+        st.push(i);
+    }
+        
+    st=new Stack<>();
+    st.push(0);
+    int[] lb=new int[n];    
+    lb[0]=-1;
+        
+    for(int i=1;i<n;i++){
+        while(st.size()>0&&arr[i]<=arr[st.peek()]){
+            st.pop();
+        }
+        
+        if(st.size()==0){
+            lb[i]=-1;
+        }else{
+            lb[i]=st.peek();
+        }
+        
+        st.push(i);
+    }    
+        int max=0;
+        
+    for(int idx=0;idx<n;idx++){
+        int w=rb[idx]-lb[idx]-1;
+        int h=arr[idx];
+        
+        int ar=h*w;
+        
+        if(ar>max){
+            max=ar;
+        }
+        
+    }    
+       return max; 
     }
 }
