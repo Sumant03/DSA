999bbc056c251e075a1278d239be8bfa5acb8522
Sumant <sumant.sharma.nonu@gmail.com>
2022-02-11 20:29:29 +0530

    Time: 16 ms (44.68%), Space: 45.2 MB (18.29%) - LeetHub

---
+++ 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isValid(String s) {
+       
+        
+        Stack<Character> st=new Stack<>();
+        
+        for(int i=0;i<s.length();i++){
+            
+            char ch=s.charAt(i);
+            if(ch=='c'){
+               
+                if(st.size()>=2&&st.pop()=='b'&&st.pop()=='a'){
+                    
+                }else{
+                    return false;
+                }
+                
+                
+            }else{
+                st.push(ch);
+            }
+        }
+        
+       return st.size()==0; 
+    }
+}
