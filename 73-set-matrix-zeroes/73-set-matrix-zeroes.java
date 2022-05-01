68acc48113a68dcd4306a414e73fd164091c4288
Sumant <sumant.sharma.nonu@gmail.com>
2022-05-01 17:32:45 +0530

    Time: 1 ms (88.64%), Space: 54.1 MB (50.93%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -1,26 +1,34 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int rows = matrix.length, cols = matrix[0].length;
-  int dummy1[]=new int[rows];
-  int dummy2[]=new int[cols];
-  Arrays.fill(dummy1,-1);
-  Arrays.fill(dummy2,-1);
-  for (int i = 0; i < rows; i++) {
-    for (int j = 0; j < cols; j++) {
-      if (matrix[i][j] == 0) {
-        dummy1[i] = 0;
-        dummy2[j] = 0;
-      }
-    }
-
-  }
-  for (int i = 0; i < rows; i++) {
-    for (int j = 0; j < cols; j++) {
-      if (dummy1[i] == 0 || dummy2[j]==0) {
-        matrix[i][j] = 0;
-      }
-    }
-  }
+       
+        int row=matrix.length, col=matrix[0].length;
+        
+        int[] d1=new int[row];
+        int[] d2=new int[col];
+        
+        Arrays.fill(d1,-1);
+        Arrays.fill(d2,-1);
+        
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                
+                
+                if(matrix[i][j]==0){
+                    d1[i]=0;
+                    d2[j]=0;
+                }
+            }
+        }
+        
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                
+                
+                if(d1[i]==0||d2[j]==0){
+                    matrix[i][j]=0;
+                }
+            }
+        }
         
     }
 }
