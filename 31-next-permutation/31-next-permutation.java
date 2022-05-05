30a3d6e6fb75eec5ef72901e0145545d81501ccd
Sumant <sumant.sharma.nonu@gmail.com>
2022-05-05 17:49:35 +0530

    Time: 1 ms (82.80%), Space: 43.6 MB (65.07%) - LeetHub

--- 31-next-permutation/31-next-permutation.java
+++ 31-next-permutation/31-next-permutation.java
@@ -1,58 +1,23 @@
 class Solution {
-    public void nextPermutation(int[] nums) {
-        if(nums==null||nums.length<=1){
-            return;
+   public void nextPermutation(int[] A) {
+        if(A == null || A.length <= 1) return;
+        int i = A.length - 2;
+        while(i >= 0 && A[i] >= A[i + 1]) i--; 
+        if(i >= 0) {                           
+            int j = A.length - 1;              
+            while(A[j] <= A[i]) j--;      
+            swap(A, i, j);                     
         }
-        
-        int idx=-1;
-        
-        for(int i=nums.length-2;i>=0;i--){
-            
-            if(nums[i]<nums[i+1]){
-                idx=i;
-                 System.out.println(idx);
-                break;
-                
-            }
-           
-        }
-        
-        if(idx>=0){
-                int k=nums.length-1;  
-            for(int i=nums.length-1;i>=0;i--){
-        
-                if(nums[idx]<nums[i]){
-                    k=i;
-                      System.out.println(k);
-                    break;
-                }
-               
-        }
-        
-            swap(nums,k,idx);
-        }
-        
-        if(idx<0){
-            reverse(nums,0,nums.length-1);
-        }else{
-        
-        reverse(nums,idx+1,nums.length-1);
-        }
-        
-    }
-    
-    public void swap(int[] arr,int k,int idx){
-        int temp=arr[k];
-        arr[k]=arr[idx];
-        arr[idx]=temp;
-        
-    }
-    
-    public void reverse(int[] arr,int i,int j){
-        
-        while(i<=j){
-           swap(arr,i++,j--);
-        }
-        
-    }
+        reverse(A, i + 1, A.length - 1);      
+}
+
+public void swap(int[] A, int i, int j) {
+    int tmp = A[i];
+    A[i] = A[j];
+    A[j] = tmp;
+}
+
+public void reverse(int[] A, int i, int j) {
+    while(i < j) swap(A, i++, j--);
+}
 }
