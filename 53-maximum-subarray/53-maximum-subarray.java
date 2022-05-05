b8ab3dc7359a654c5748489337c843f0b1d0f2ed
Sumant <sumant.sharma.nonu@gmail.com>
2022-05-05 18:40:30 +0530

    Time: 1 ms (100.00%), Space: 73.5 MB (51.97%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -1,17 +1,17 @@
 class Solution {
     public int maxSubArray(int[] arr) {
-        int csum=arr[0];
+        int csum=0;
         int msum=arr[0];
         
-        for(int idx=1;idx<arr.length;idx++){
+        for(int idx=0;idx<arr.length;idx++){
             
-            
-            if(csum<0){
+            csum+=arr[idx];
+           
+            msum=Math.max(csum,msum);
+             if(csum<0){
                 csum=0;
             }
-            csum+=arr[idx];
-            msum=Math.max(csum,msum);
-            
+             
         }
         
         
