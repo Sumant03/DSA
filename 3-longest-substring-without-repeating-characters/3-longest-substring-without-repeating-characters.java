e17f33c4a9077ceb9347ce78fe03782e258029b9
Sumant <sumant.sharma.nonu@gmail.com>
2022-06-10 14:47:52 -0500

    Time: 9 ms (67.13%), Space: 45.2 MB (39.88%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -4,25 +4,18 @@ class Solution {
             return 1;
         } 
         
-        int max=0;
-        int left=0;
-        HashSet<Character> set=new HashSet<>();
-        
-        for(int right=0;right<s.length();right++){
-            
-            char ch=s.charAt(right);
-            if(set.contains(ch)){
-                System.out.println(right +' '+left);
-                while(left<right&&set.contains(ch)){
-                    set.remove(s.charAt(left));
-                    left++;
-                }
-                    
-            }
-             set.add(ch);
-             max=Math.max(max,right-left+1);
+                HashMap < Character, Integer > mpp = new HashMap < Character, Integer > ();
+        int left = 0, right = 0;
+        int n = s.length();
+        int len = 0;
+        while (right < n) {
+            if (mpp.containsKey(s.charAt(right))) left = Math.max(mpp.get(s.charAt(right)) + 1, left);
+
+            mpp.put(s.charAt(right), right);
+
+            len = Math.max(len, right - left + 1);
+            right++;
         }
-        
-        return max;
+        return len;
     }
 }
